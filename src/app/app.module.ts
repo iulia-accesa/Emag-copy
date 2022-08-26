f96e9acfb70352421f795125fd453e453255322f
ACCESA\iulia.filimon <Iulia.Filimon@accesa.eu>
2022-08-26 14:15:12 +0300

    added ngrx effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from 'src/environments/environment';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { StoreModule } from "@ngrx/store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { environment } from "src/environments/environment";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SearchEngineModule } from "./search-engine/search-engine.module";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SearchBarComponent } from './search-engine/search-bar/search-bar.component';
-import { SearchEngineModule } from './search-engine/search-engine.module';
 
 @NgModule({
   declarations: [
