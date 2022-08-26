f5c0fb86678d6d36535706b23ac7727ed2f55586
ACCESA\iulia.filimon <Iulia.Filimon@accesa.eu>
2022-08-26 10:01:27 +0300

    added ngrx folder hierarchy structure

---
+++ src/app/search-engine/my-ngrx/features.ts
@@ -0,0 +1,1 @@
+export const searchResultFeatureKey = 'searchResult';
---
+++ src/app/search-engine/my-ngrx/states/states.ts
@@ -0,0 +1,6 @@
+
+import {SearchBarProduct} from '../../models/search-bar.product'
+export interface SearchResultState{
+    searchResult:SearchBarProduct[]
+}
+
