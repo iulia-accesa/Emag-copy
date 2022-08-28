44f34f3de64a365a4f7fdd06c7e62edff1340baa
ACCESA\iulia.filimon <Iulia.Filimon@accesa.eu>
2022-08-28 19:14:35 +0300

    made to react to the event once,i have to work on effect

--- src/app/search-engine/my-ngrx/effects/index.ts
+++ src/app/search-engine/my-ngrx/effects/index.ts
@@ -1,37 +1,30 @@
-import { Injectable } from "@angular/core";
-import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, Effect, ofType } from '@ngrx/effects';
 import * as fromSearchActions from '../actions/index';
-import { tap,take } from "rxjs";
-import { SearchBarService } from "../../services/search-bar.service";
-import { SearchBarProduct } from "../../models/search-bar.product";
+import { tap, take, switchMap, map } from 'rxjs';
+import { SearchBarService } from '../../services/search-bar.service';
+import { SearchBarProduct } from '../../models/search-bar.product';
 @Injectable()
 export class SearchEffects {
 
-    inputChanged$ = createEffect(() => 
-            this._actions$
-            .pipe(
-                
-                take(1),
-                ofType(fromSearchActions.inputChanged),
-                
-                tap(action =>{
-                   
-                    let result:SearchBarProduct[] = [{id:1,title:"mens",category:"mesn",rating:{rate:0,count:0}}];
-                    //  this._searchBarService.getProductsForSearchBar(action.input)
-                    //  .then(response => {
-                    //     result = response;
-                    //  });
-                     console.log(result)
-                  fromSearchActions.updateSearchResult({payload:result})
-                   
-                })
+  inputChanged$ = createEffect(() =>
+    this._actions$.pipe(
+      take(1),
+      ofType(fromSearchActions.inputChanged),
+      switchMap( action =>
+        this._searchBarService
+          .getProductsForSearchBar(action.input)
+          .pipe(
+            map((obsProducts: SearchBarProduct[]) =>
+              fromSearchActions.updateSearchResult({ payload: obsProducts })
             )
-    );
+          )
+      )
+    )
+  );
 
-
-    constructor(private _actions$: Actions,private _searchBarService: SearchBarService){
-
-    }
-
-
+  constructor(
+    private _actions$: Actions,
+    private _searchBarService: SearchBarService
+  ) {}
 }
--- src/app/search-engine/services/search-bar.service.ts
+++ src/app/search-engine/services/search-bar.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, take } from 'rxjs';
-import { Product } from '../models/product';
+import { map, Observable, race, take } from 'rxjs';
+import { Product, ProductRating } from '../models/product';
 import { SearchBarProduct } from '../models/search-bar.product';
 import * as fromApi from '../resources/api-endpoints';
 
@@ -14,27 +14,24 @@ export class SearchBarService {
   }
 
   //populates the store with the available products for search
-  getProductsForSearchBar(text: string) :Promise<SearchBarProduct[]>{
-    return new Promise(resolve =>
-    this.getAllProducts()
-    .pipe(take(1))
-    .subscribe((products: Product[]) => {
-      let searchResult: SearchBarProduct[] =
-       products
+  getProductsForSearchBar(text: string):Observable<SearchBarProduct[]> {
+    return this.getAllProducts().pipe(
+      take(1),
+      map((products: Product[]) => products
         .filter(product => product.title.toLowerCase().startsWith(text))
-        .map(product => {
-          let searchProduct: SearchBarProduct = {
-            id: product.id,
-            title: product.title,
-            category: product.category,
-            rating: product.rating,
-          };
-          return searchProduct;
-        });
-       console.log(searchResult);
-       resolve(searchResult)
-    }));
-
-
+        .map(product =>
+            {
+            let searchProduct: SearchBarProduct = {id:product.id,
+              title:product.title,
+              category:product.category,
+              rating:product.rating
+            }
+            return searchProduct;
+          }
+        
+        )
+      )
+    );
+    
   }
 }
