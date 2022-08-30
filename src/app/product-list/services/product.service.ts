b191402600b22d08e812d4496260a841e5ea6652
ACCESA\istvan.iodi <istvan.iodi@accesa.eu>
2022-08-30 12:57:07 +0300

    product filter section UI bug fixed

--- src/app/product-list/services/product.service.ts
+++ src/app/product-list/services/product.service.ts
@@ -2,16 +2,38 @@ import { ProductModel } from './../models/product.model';
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { max, Observable, of } from 'rxjs';
 
 @Injectable()
 export class ProductService {
   private readonly baseURL = 'https://fakestoreapi.com/';
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    console.log(this.getPriceRange());
+  }
 
   getAll(): Observable<ProductModel[]> {
     return this.http.get(`${this.baseURL}products`) as Observable<
       ProductModel[]
     >;
+  }
+
+  getPriceRange() {
+    this.getAll().subscribe((products: ProductModel[]) => {
+      let priceRange = { min: 1, max: 19999999 };
+
+      let minPrice = products[0].price;
+      let maxPrice = minPrice;
+
+      products.forEach((product) => {
+        console.log(product);
+        if (product.price < minPrice) minPrice = product.price;
+        if (product.price > maxPrice) maxPrice = product.price;
+      });
+
+      priceRange.min = minPrice;
+      priceRange.max = maxPrice;
+
+      return of(priceRange);
+    });
   }
 }
