7149a0b0797ff71cd184719cd5fd6de62e99cfba
ACCESA\iulia.filimon <Iulia.Filimon@accesa.eu>
2022-08-31 12:37:45 +0300

    reset develop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 import { environment } from "src/environments/environment";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { SearchEngineModule } from "./search-engine/search-engine.module";
+
 
 
 @NgModule({
@@ -17,7 +17,7 @@ import { SearchEngineModule } from "./search-engine/search-engine.module";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SearchEngineModule,
+    
     StoreModule.forRoot({}, {}),
     StoreDevtoolsModule.instrument({maxAge: 25, logOnly: environment.production}),
     EffectsModule.forRoot([])
--- src/app/search-engine/resources/api-endpoints.ts
+++
@@ -1,1 +0,0 @@
-export const ALL_PRODUCTS = 'https://fakestoreapi.com/products';
