0618406fdfcf39ad546d9b34ede8290b91970273
ACCESA\iulia.filimon <Iulia.Filimon@accesa.eu>
2022-08-31 15:14:35 +0300

    excluded user.class.ts and refactored url's look

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,18 @@ import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 import { environment } from "src/environments/environment";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { ProductCardComponent } from "./shared/components/product-card/product-card.component";
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,ProductCardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    
+  
     StoreModule.forRoot({}, {}),
     StoreDevtoolsModule.instrument({maxAge: 25, logOnly: environment.production}),
     EffectsModule.forRoot([])
