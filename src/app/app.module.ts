34c0525180d98fc11b2a00f870ab38b339bc6e0a
ACCESA\iurii.armas <iurii.armas@accesa.eu>
2022-08-31 12:23:19 +0300

    Updated app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,50 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetailsPageComponent } from './details-page/details-page.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductService } from './product.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
-import { environment } from 'src/environments/environment';
+import { environment } from '../environments/environment';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { ProductPhotosComponent } from './details-page/product-photos/product-photos.component';
+import { AddToCartBoxComponent } from './details-page/add-to-cart-box/add-to-cart-box.component';
+import { SimilarProductComponent } from './details-page/similar-product/similar-product.component';
+import { ProductCardComponent } from './shared/product-card/product-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsPageComponent,
+    ProductPhotosComponent,
+    AddToCartBoxComponent,
+    SimilarProductComponent,
+    ProductCardComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    CommonModule,
     StoreModule.forRoot({}, {}),
-    StoreDevtoolsModule.instrument({maxAge: 25, logOnly: environment.production}),
-    EffectsModule.forRoot([])
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
+    EffectsModule.forRoot([]),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ProductService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
