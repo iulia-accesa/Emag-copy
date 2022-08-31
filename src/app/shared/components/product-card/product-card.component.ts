541e761aae0cced777641a7276f25b2e7798e643
ACCESA\iulia.filimon <Iulia.Filimon@accesa.eu>
2022-08-31 14:10:31 +0300

    added product-card component in shared folder

--- src/app/shared/components/product-card/product-card.component.ts
+++ src/app/shared/components/product-card/product-card.component.ts
@@ -1,1 +1,18 @@
-//TODO replace by real component
+import { Component, Input } from '@angular/core';
+import { IProductApi } from '../../models/product-api.interface';
+
+
+@Component({
+  selector: 'product-card-component',
+  templateUrl: './product-card.component.html',
+  styleUrls: ['./product-card.component.scss'],
+})
+export class ProductCardComponent {
+  @Input() product: IProductApi | undefined;
+
+  constructor() {}
+
+  calcRating(rat: number) {
+    return Math.round(rat);
+  }
+}
