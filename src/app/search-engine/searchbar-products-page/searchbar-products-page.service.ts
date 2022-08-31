4d7f4d64a944cab43b5bb9901d9d8f8c12bae12a
ACCESA\iulia.filimon <Iulia.Filimon@accesa.eu>
2022-08-31 09:38:52 +0300

    products page service uses the shared interface IProduct now

--- src/app/search-engine/searchbar-products-page/searchbar-products-page.service.ts
+++ src/app/search-engine/searchbar-products-page/searchbar-products-page.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable,take,map } from "rxjs";
-import { Product } from "../models/product";
+import { IProduct } from "src/app/shared/product-card/product-card.component.interface";
 import { ALL_PRODUCTS } from "../resources/api-endpoints";
 
 @Injectable()
@@ -9,12 +9,12 @@ export class SearchbarProductsPageService {
 
     constructor(private http: HttpClient) {}
 
-    private getAllProducts(): Observable<Product[]> {
-        return this.http.get<Product[]>(ALL_PRODUCTS);
+    private getAllProducts(): Observable<IProduct[]> {
+        return this.http.get<IProduct[]>(ALL_PRODUCTS);
       }
 
 
-    private productsMatchesSearchKey(product: Product,searchKey: string) :boolean {
+    private productsMatchesSearchKey(product: IProduct,searchKey: string) :boolean {
             return searchKey !== "" && 
                                 (product.title.toLowerCase().startsWith(searchKey.toLowerCase()) || 
                                 product.title.toLowerCase().split(" ").includes(searchKey.toLowerCase()) ||
@@ -23,10 +23,10 @@ export class SearchbarProductsPageService {
                                 );
     }  
 
-    filterProductsBySearchKey(searchKey: string): Observable<Product[]>{
+    filterProductsBySearchKey(searchKey: string): Observable<IProduct[]>{
         return this.getAllProducts().pipe(
             take(1),
-            map((products: Product[]) => 
+            map((products: IProduct[]) => 
                products.filter(product => this.productsMatchesSearchKey(product,searchKey) )
             )
         );
