42ead6425fa5e662cc0597fe6de4e48e372ad744
ACCESA\iurii.armas <iurii.armas@accesa.eu>
2022-08-31 16:07:28 +0300

    Product Card commit

--- src/app/shared/components/product-card/product-card.component.ts
+++ src/app/shared/components/product-card/product-card.component.ts
@@ -1,1 +1,17 @@
-//TODO replace by real component
+import { Component, Input } from '@angular/core';
+import { IProduct } from './product-card.component.interface';
+
+@Component({
+  selector: 'product-card-component',
+  templateUrl: './product-card.component.html',
+  styleUrls: ['./product-card.component.scss'],
+})
+export class ProductCardComponent {
+  @Input() product: IProduct | undefined;
+
+  constructor() {}
+
+  calcRating(rat: number) {
+    return Math.round(rat);
+  }
+}
