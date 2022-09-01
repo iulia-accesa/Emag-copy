49444b7dfc69c681ddaf8967d6f515a388fdd38e
ACCESA\iulia.filimon <Iulia.Filimon@accesa.eu>
2022-09-01 14:53:40 +0300

    modified product-list-search-engine folder

--- src/app/product-list-search-engine/product-list-search-engine.component.ts
+++ src/app/product-list-search-engine/product-list-search-engine.component.ts
@@ -1,15 +1,1 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-product-list-search-engine',
-  templateUrl: './product-list-search-engine.component.html',
-  styleUrls: ['./product-list-search-engine.component.scss']
-})
-export class ProductListSearchEngineComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+// TODO: replace by real component
