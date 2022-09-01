b44a1ddef13f5b641fb721cc2196b0db99115be6
ACCESA\iulia.filimon <Iulia.Filimon@accesa.eu>
2022-09-01 14:47:13 +0300

    added homepage and product-list-search-engine components structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductCardComponent } from './shared/components/product-card/product-card.component';
+import { HomepageModule } from './homepage/homepage.module';
+import { ProductListSearchEngineComponent } from './product-list-search-engine/product-list-search-engine.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductCardComponent],
+  declarations: [AppComponent, ProductListSearchEngineComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    
+    HomepageModule,
 
     StoreModule.forRoot({}, {}),
     StoreDevtoolsModule.instrument({
---
+++ src/app/product-list-search-engine/product-list-search-engine.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list-search-engine',
+  templateUrl: './product-list-search-engine.component.html',
+  styleUrls: ['./product-list-search-engine.component.scss']
+})
+export class ProductListSearchEngineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
