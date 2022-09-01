4241b1c7921e16462b63c1cb5d6a57f4fe85a96d
ACCESA\iulia.filimon <Iulia.Filimon@accesa.eu>
2022-09-01 14:52:35 +0300

    modified app.module and app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
-import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent /* HomeComponent */ },
+  { path: '', component: HomepageComponent },
   // { path: 'login', component: LoginComponent },
   // { path: 'products', component: ProductListPageComponent },
   // { path: 'products/:id', component: DetailsPageComponent }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,10 @@ import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageModule } from './homepage/homepage.module';
-import { ProductListSearchEngineComponent } from './product-list-search-engine/product-list-search-engine.component';
+
 
 @NgModule({
-  declarations: [AppComponent, ProductListSearchEngineComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
