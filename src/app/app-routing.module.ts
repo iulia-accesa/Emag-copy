aa79a9c21a0fdcfc342eaa82458ed9c48773f479
ACCESA\iulia.filimon <Iulia.Filimon@accesa.eu>
2022-09-05 16:33:05 +0300

    removed product-list-search-engine component and service,no longer neded,coupled searchbar component to product-detail component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { ProductListSearchEngineComponent } from './product-list-search-engine/product-list-search-engine.component';
 import { LoginComponent } from './account/login/login.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { SearchBarComponent } from './shared/components/header/search/searchbar/searchbar.component';
 
 const routes: Routes = [
-  { path: '', component: HomepageComponent },
+  { path: '', component: SearchBarComponent },
   { path: 'login', component: LoginComponent },
   // { path: 'products', component: ProductListPageComponent },
   //{ path: 'products/:id', component: DetailsPageComponent }
-  { path: 'search/products', component: ProductListSearchEngineComponent },
 ];
 
 @NgModule({
