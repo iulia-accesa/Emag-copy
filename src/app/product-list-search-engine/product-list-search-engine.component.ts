ba966998783e77b76ab95f26358ba3b8129e7a81
ACCESA\iulia.filimon <Iulia.Filimon@accesa.eu>
2022-09-05 08:07:45 +0300

    modified checkForResults function,added productsSubscription variable

--- src/app/product-list-search-engine/product-list-search-engine.component.ts
+++ src/app/product-list-search-engine/product-list-search-engine.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { first, Observable, of } from 'rxjs';
+import { first, Observable, of, Subscription } from 'rxjs';
 import { ProductListSearchEngineService } from '../services/product-list-search-engine/product-list-search-engine.service';
 
 import { IProductApi } from '../shared/models/product-api.interface';
@@ -10,14 +10,18 @@ import { IProductApi } from '../shared/models/product-api.interface';
   templateUrl: './product-list-search-engine.component.html',
   styleUrls: ['./product-list-search-engine.scss'],
 })
-export class ProductListSearchEngineComponent implements OnInit {
+export class ProductListSearchEngineComponent implements OnInit,OnDestroy {
   private _searchKey: string = '';
   products$: Observable<IProductApi[]> = of([]);
   anyResults: boolean = true;
+  productsSubscription = new Subscription();
   constructor(
     private _route: ActivatedRoute,
     private _service: ProductListSearchEngineService
   ) {}
+  ngOnDestroy(): void {
+    this.productsSubscription.unsubscribe();
+  }
 
   ngOnInit(): void {
     this._route.queryParams.pipe(first()).subscribe((params) => {
@@ -33,7 +37,7 @@ export class ProductListSearchEngineComponent implements OnInit {
    * Checks if there is a match in the product database for the given key and modifies  @var anyResult
    */
   private checkForResults() {
-    this.products$.pipe(first()).subscribe((val) => {
+    this.productsSubscription = this.products$.subscribe((val) => {
       if (val.length === 0) {
         this.anyResults = false;
       } else {
