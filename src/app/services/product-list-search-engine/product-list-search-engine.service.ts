a853e664ea12536a2dd01df3fad5edbcd03d9e36
ACCESA\iulia.filimon <Iulia.Filimon@accesa.eu>
2022-09-05 08:13:23 +0300

    formated product-list-search-engine.service.ts

--- src/app/services/product-list-search-engine/product-list-search-engine.service.ts
+++ src/app/services/product-list-search-engine/product-list-search-engine.service.ts
@@ -1,32 +1,40 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 
-import { Observable,take,map } from "rxjs";
-import { ProductApiService } from "src/app/services/product-api.service";
-import { IProductApi } from "src/app/shared/models/product-api.interface";
-
-
+import { Observable, take, map } from 'rxjs';
+import { ProductApiService } from 'src/app/services/product-api.service';
+import { IProductApi } from 'src/app/shared/models/product-api.interface';
 
 @Injectable()
 export class ProductListSearchEngineService {
+  constructor(private _productApiService: ProductApiService) {}
 
-    constructor(private _productApiService: ProductApiService) {}
+  private productsMatchesSearchKey(
+    product: IProductApi,
+    searchKey: string
+  ): boolean {
+    return (
+      searchKey !== '' &&
+      (product.title.toLowerCase().startsWith(searchKey.toLowerCase()) ||
+        product.title
+          .toLowerCase()
+          .split(' ')
+          .includes(searchKey.toLowerCase()) ||
+        product.description
+          .toLowerCase()
+          .split(' ')
+          .includes(searchKey.toLowerCase()))
+    );
+  }
 
-
-    private productsMatchesSearchKey(product: IProductApi,searchKey: string) :boolean {
-            return searchKey !== "" && 
-                                (product.title.toLowerCase().startsWith(searchKey.toLowerCase()) || 
-                                product.title.toLowerCase().split(" ").includes(searchKey.toLowerCase()) ||
-                                product.description.toLowerCase().split(" ").includes(searchKey.toLowerCase()) 
-                                
-                                );
-    }  
-
-    filterProductsBySearchKey$(searchKey: string): Observable<IProductApi[]>{
-        return this._productApiService.getAll().pipe(
-            map((products: IProductApi[]) => 
-               products.filter(product => this.productsMatchesSearchKey(product,searchKey) )
-            )
-        );
-    }
-    
+  filterProductsBySearchKey$(searchKey: string): Observable<IProductApi[]> {
+    return this._productApiService
+      .getAll()
+      .pipe(
+        map((products: IProductApi[]) =>
+          products.filter((product) =>
+            this.productsMatchesSearchKey(product, searchKey)
+          )
+        )
+      );
+  }
 }
