f34f4906b34b50cf0c2a8f90d1be20cf5f8c4143
Andrei Vasilescu <andrei.vasilescu@accesa.eu>
2022-09-06 15:58:17 +0300

    Modified some scss to remove extra padding and bpl

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { FormControl } from '@angular/forms';
-
-import { Observable } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -11,6 +7,7 @@ import { map, startWith } from 'rxjs/operators';
   encapsulation: ViewEncapsulation.None,
 })
 export class HeaderComponent implements OnInit {
+
   constructor() {}
 
   ngOnInit(): void {}
