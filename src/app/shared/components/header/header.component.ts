029d41f8f76b0f0e8eda8e7f80a40b9a410cec9c
Andrei Vasilescu <andrei.vasilescu@accesa.eu>
2022-09-06 10:48:15 +0300

    Removed ts for header / removed html dependencies

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -11,24 +11,7 @@ import { map, startWith } from 'rxjs/operators';
   encapsulation: ViewEncapsulation.None,
 })
 export class HeaderComponent implements OnInit {
-  myControl = new FormControl('');
-  options: string[] = ['One', 'Two', 'Three'];
-  filteredOptions!: Observable<string[]>;
-  mobile: boolean = false;
-  filled: boolean = false;
   constructor() {}
 
-  ngOnInit(): void {
-    this.filteredOptions = this.myControl.valueChanges.pipe(
-      startWith(''),
-      map((value) => this._filter(value || ''))
-    );
-  }
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-
-    return this.options.filter((option) =>
-      option.toLowerCase().includes(filterValue)
-    );
-  }
+  ngOnInit(): void {}
 }
